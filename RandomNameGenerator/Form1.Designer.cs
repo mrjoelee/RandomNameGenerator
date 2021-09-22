377b624e87171f72379df0327b2905237d563685
mrjoelee <90262428+mrjoelee@users.noreply.github.com>
2021-09-22 01:35:45 -0400

    Add project files.

---
+++ RandomNameGenerator/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿
+namespace RandomNameGenerator
+{
+    partial class RandomNameGenerator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RandomNameGenerator));
+            this.button1 = new System.Windows.Forms.Button();
+            this.listbox1 = new System.Windows.Forms.ListBox();
+            this.listbox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(248, 327);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(163, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Random";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listbox1
+            // 
+            this.listbox1.FormattingEnabled = true;
+            this.listbox1.ItemHeight = 20;
+            this.listbox1.Location = new System.Drawing.Point(34, 57);
+            this.listbox1.Name = "listbox1";
+            this.listbox1.Size = new System.Drawing.Size(235, 264);
+            this.listbox1.TabIndex = 1;
+            this.listbox1.SelectedIndexChanged += new System.EventHandler(this.listbox1_SelectedIndexChanged);
+            // 
+            // listbox2
+            // 
+            this.listbox2.FormattingEnabled = true;
+            this.listbox2.ItemHeight = 20;
+            this.listbox2.Location = new System.Drawing.Point(396, 57);
+            this.listbox2.Name = "listbox2";
+            this.listbox2.Size = new System.Drawing.Size(224, 264);
+            this.listbox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(29, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Names";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(391, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 26);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Result";
+            // 
+            // RandomNameGenerator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(642, 368);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listbox2);
+            this.Controls.Add(this.listbox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "RandomNameGenerator";
+            this.Text = "RandomNameGenerator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listbox1;
+        private System.Windows.Forms.ListBox listbox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ RandomNameGenerator/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RandomNameGenerator
+{
+    public partial class RandomNameGenerator : Form
+    {
+        List<string> l1;
+  
+        public RandomNameGenerator()
+        {
+            InitializeComponent();
+            l1 = new List<string>();
+            l1.Add("Scott");
+            l1.Add("Jay");
+            l1.Add("Torrie");
+            l1.Add("Dennis");
+            l1.Add("Joshua");
+            l1.Add("Joe");
+            l1.Add("Michael");
+            l1.Add("Daniel");
+            l1.Add("Billy");
+            l1.Add("Coung");
+            l1.Add("Erick");
+            l1.Add("Lumiere");
+            l1.Add("Austin");
+            listbox1.DataSource = l1;
+            
+        }
+        private int EatMyRandom()
+        {
+            Random random = new Random();
+            return random.Next(0,13);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listbox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+    }
+}
